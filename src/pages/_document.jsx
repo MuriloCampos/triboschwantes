d2c8922ba14760584175553d61ee7b862360f4b5
Murilo Campos <murilo13campos@gmail.com>
2021-06-04 10:19:04 -0400

    fixing document bug

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/Document'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -24,22 +24,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
-  }
-
-  render() {
-    return(
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-
-          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
   }
 }
